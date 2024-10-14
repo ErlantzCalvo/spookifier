3864c950e8b4903dbd68ac50a837da9513c7c72d
Erlantz Calvo <erlantz.calvo@incotel.es>
2024-10-14 17:58:42 +0200

    Increase logo font size

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,14 +40,14 @@ function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
         <Link href="/" className="flex items-center">
           {/* <CircleIcon className="h-6 w-6 text-orange-500" /> */}
-          <span className=" ml-2 text-xl font-semibold text-orange-600 font-[family-name:var(--font-october-crow)]">SPOOKIFIER</span>
+          <span className=" ml-2 text-2xl font-semibold text-orange-600 font-[family-name:var(--font-october-crow)]">SPOOKIFIER</span>
         </Link>
         <div className="flex items-center space-x-4">
           <Link
-            href="/pricing"
+            href="/about"
             className="text-sm font-medium text-gray-700 hover:text-gray-900"
           >
-            Pricing
+            About
           </Link>
           
         </div>
