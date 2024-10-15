06d9e73585bd4840a45e6950ec69813b1e82d63b
Erlantz Calvo <erlantz.calvo@incotel.es>
2024-10-15 17:58:20 +0200

    Add get user photos api path

---
+++ app/api/getUserPhotos/route.ts
@@ -0,0 +1,25 @@
+import {v2 as cloudinaryApi} from 'cloudinary'
+
+export async function GET(request: Request) {
+    const {searchParams} = new URL(request.url)
+    const uid = searchParams.get('uid')
+    if(!uid) return new Response('UID parameter is needed', {
+        status: 400,
+        statusText: "MISSING_UID"
+    })
+
+    if(uid.length !== 36) return new Response('Invalid UID value', {
+        status: 400,
+        statusText: "INVALID_UID"
+    })
+
+    const userImages = await cloudinaryApi.search
+    // .expression(`resource_type:image AND asset_folder:spookifier AND public_id:${uid}/*`)
+    .expression(`resource_type:image `)
+    .sort_by('public_id','desc')
+    .max_results(300)
+    .execute()
+    
+
+    return Response.json({data: userImages})
+}
